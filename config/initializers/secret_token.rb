5734ec69164646e1c8515592edd426afbb31c8e8
unknown <kim@.(none)>
2011-03-29 19:46:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lamp::Application.config.secret_token = 'bcdfa710207553b8b156889d28ac9fb616b7dd84dd5bc50834e1c419f65fb745a408e2d47883c3ff1de37315be9e521cb7d51458dbc4776c1ba59720564c0394'
